1145ebedaf60e354714033839e513148fa4533ce
Kaitlyn Sullivan <sullivank210@gmail.com>
2023-02-08 20:12:11 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript.
             </header>
+            <p>Owner: Kaitlyn Sullivan</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
