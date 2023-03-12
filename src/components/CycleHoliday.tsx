347d12ba4f0628dabffcd72a9a29692f1272ac2e
Kaitlyn Sullivan <sullivank210@gmail.com>
2023-03-11 20:54:53 -0500

    Finished cycleHoliday with no errors

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,37 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+type Holiday = "🎄" | "🎃" | "🦃" | "🐰🥚" | "❤️";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("❤️");
+    function nextYearly(): void {
+        const yearlyOrder: Array<Holiday> = ["❤️", "🐰🥚", "🎃", "🦃", "🎄"];
+        const i: number = yearlyOrder.findIndex(
+            (current: Holiday): boolean => current === holiday
+        );
+        if (i === 4) {
+            setHoliday(yearlyOrder[0]);
+        } else {
+            setHoliday(yearlyOrder[i + 1]);
+        }
+    }
+    function nextABC(): void {
+        const ABCorder: Array<Holiday> = ["🎄", "🐰🥚", "🎃", "🦃", "❤️"];
+        const i: number = ABCorder.findIndex(
+            (current: Holiday): boolean => current === holiday
+        );
+        if (i === 4) {
+            setHoliday(ABCorder[0]);
+        } else {
+            setHoliday(ABCorder[i + 1]);
+        }
+    }
+    return (
+        <div>
+            <div>Cycle Holiday</div>
+            <Button onClick={nextYearly}>Advance Yearly</Button>
+            <Button onClick={nextABC}>Advance Alphabetically</Button>
+            Holiday: {holiday};
+        </div>
+    );
 }
