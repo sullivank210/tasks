977424525d015bfdd411328906abcbd58572501a
Kaitlyn Sullivan <sullivank210@gmail.com>
2023-03-15 22:04:44 -0400

    Check answer passes all tests and shows up on page

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
-/*
-The CheckAnswer component allows a user to enter an answer to a short answer question, and then displays whether they got it correct or incorrect. The component takes in a parameter representing the expectedAnswer.
-
-You will need a state to handle the user’s given answer
-The user’s given answer should initially be the empty string
-When the user’s given answer matches the expectedAnswer, display ✔️; otherwise display ❌
-*/
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
+            <Form.Group controlId="formAnswer">
+                <Form.Label>Enter answer:</Form.Label>
+                <Form.Control value={answer} onChange={updateAnswer} />
+            </Form.Group>
             <h3>Check Answer</h3>
+            <div>{answer === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
