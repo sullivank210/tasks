8c92a2e47dd3c6eb4b9650c573b2cfc2d0da1aa0
Kaitlyn Sullivan <sullivank210@gmail.com>
2023-03-16 14:13:35 -0400

    Finished change color. Passes all tests and displays on page

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,24 +1,59 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
-/*
-The ChangeColor component offers the user a bunch of radio buttons labeled with different colors; choosing a radio 
-button updates the text and color of a nearby box of text.
+const COLORS = [
+    "FireBrick",
+    "LightCoral",
+    "Blue",
+    "LightBlue",
+    "BlueViolet",
+    "DarkSeaGreen",
+    "LightGreen",
+    "LightSalmon"
+];
 
-- You will need state to represent the chosen color
-- You are free to make up your own list of colors, but you must have AT LEAST 8 unique colors.
-- Each color must be represented by a radio button.
-- There should be a box of text with a data-testid="colored-box" attribute and value.
-- The box of text should have the same text and backgroundColor style as the currently selected radio button.
-
-HINT: We strongly recommend you use map to render all the color’s Form.Check tags rather than hardcoding each one!
-HINT: We recommend you use the inline attribute for the radio buttons.
-*/
+function ColorBox({ color }: { color: string }): JSX.Element {
+    return (
+        <div
+            data-testid="colored-box"
+            style={{
+                width: "120px",
+                height: "50px",
+                backgroundColor: color,
+                display: "inline-block",
+                verticalAlign: "bottom",
+                marginLeft: "10px"
+            }}
+        >
+            {color}
+        </div>
+    );
+}
 
 export function ChangeColor(): JSX.Element {
+    const [color, setColor] = useState<string>(COLORS[0]);
+
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
+    }
+
     return (
         <div>
             <h3>Change Color</h3>
+            {COLORS.map((chosen: string) => (
+                <Form.Check
+                    key={chosen}
+                    inline
+                    type="radio"
+                    name="color"
+                    onChange={updateColor}
+                    id={"responce-" + chosen}
+                    label={chosen}
+                    value={chosen}
+                    checked={color === chosen}
+                ></Form.Check>
+            ))}
+            <ColorBox color={color}></ColorBox>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -2,21 +2,6 @@ import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 
-/*
-The GiveAttempts component simulates a user taking a quiz with a limited number of attempts, but also provides a way 
-for them to gain as many attempts as they want via a numeric input box.
-
-- You will need a state to represent the number of attempts the user has left, and another state to represent 
-    the number of attempts they are requesting.
-- The initial number of attempts left should be 3.
-- The number of attempts left should be visible.
-- There should be a numeric input box where the user can specify their requested number of attempts.
-- There should be two buttons, one labeled use that decreases the attempts by one and one labeled gain that increases 
-    the attempts by the amount in the input box.
-- If the user attempts to request an invalid amount (e.g., the empty string "") that cannot be parsed as an integer, 
-    then do not change their number of attempts.
-- When the user is out of attempts, the use button should be disabled
-*/
 export function GiveAttempts(): JSX.Element {
     const [remaining, setRemaining] = useState<number>(3);
     const [requested, setRequested] = useState<string>("0");
